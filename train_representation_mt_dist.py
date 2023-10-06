b688c78319f20abb56075bdd147df496261c5fa6
chinganc <chinganc0@gmail.com>
2023-10-06 16:15:36 -0700

    Fix some bugs and add some features.
    1. Fix a bug of replay buffer depending on the # of gpus.
    2. Turn of MP if there is only one gpu.
    3. Add log_root_dir in config. Update the job yaml to mirror logs to ../results.
    4. Remove hardcoded a_dim and img_res; they're now specified in config yaml.
    5. Fix a bug of replay buffer deleting data files.

--- train_representation_mt_dist.py
+++ train_representation_mt_dist.py
@@ -71,9 +71,9 @@ class Workspace:
 
         device_ids = list(range(torch.cuda.device_count()))
         self.device = device_ids[rank]
-        
-        a_dim = 4
-        obs_shape = (3*self.cfg.frame_stack,84,84)
+
+        a_dim = self.cfg.action_dim
+        obs_shape = [3*self.cfg.frame_stack]+self.cfg.img_res  #(3*self.cfg.frame_stack,84,84)
         self.agent = make_agent(obs_shape,
                                 a_dim,
                                 rank,
@@ -96,8 +96,9 @@ class Workspace:
                                     self.cfg.action_repeat, self.cfg.seed, train=False)
 
         #### Don't need to load the data in the second stage (calculating BPE)
+        assert self.cfg.stage in [1, 2, 3], "Stage must be 1, 2, or 3."
         if self.cfg.stage == 2:
-            return 
+            return
         self.setup_replay_buffer()
 
 
@@ -115,9 +116,9 @@ class Workspace:
                 self.cfg.batch_size//self.world_size, self.cfg.replay_buffer_num_workers,
                 True, self.cfg.nstep, self.cfg.discount, self.rank, self.world_size)
         elif self.cfg.stage == 3:
-             downstream_data_path = construct_task_data_path(self.cfg.data_storage_dir, self.cfg.downstream_task_name, self.cfg.task_data_dir_suffix)
-             print(f"Loading target task data from {downstream_data_path}")
-             self.replay_loader = make_replay_loader_dist(
+            downstream_data_path = construct_task_data_path(self.cfg.data_storage_dir, self.cfg.downstream_task_name, self.cfg.task_data_dir_suffix)
+            print(f"Loading target task data from {downstream_data_path}")
+            self.replay_loader = make_replay_loader_dist(
                 [downstream_data_path], self.cfg.max_traj_per_task, self.cfg.replay_buffer_size,
                 self.cfg.batch_size//self.world_size, self.cfg.replay_buffer_num_workers,
                 True, self.cfg.nstep, self.cfg.discount, self.rank, self.world_size,
@@ -126,7 +127,7 @@ class Workspace:
         else:
             assert self.cfg.stage != 2, "You shouldn't set up the replay buffer for stage 2. Most likely you ended up here due to a logic bug."
 
-        print('Finish Reading Data') 
+        print('Finish Reading Data')
         self._replay_iter = None
         self.performance = []
 
@@ -148,17 +149,17 @@ class Workspace:
     def act(self, obs, code_buffer):
         obs = torch.from_numpy(obs).to(self.device)
         z = self.agent.encoder(obs.unsqueeze(0))
-        
+
         if len(code_buffer) == 0:
             ### query the meta/option policy
-            meta_action = self.agent.TACO.module.meta_policy(z).max(-1)[1] 
+            meta_action = self.agent.TACO.module.meta_policy(z).max(-1)[1]
             tok = self.idx_to_tok[int(meta_action.item())]
             try:
                 code_buffer = self.tokenizer.decode([tok], verbose=False)
             except:
                 print('Error occured when choosing meta action:{}'.format(meta_action))
                 assert False
-        
+
         code_selected = code_buffer.pop(0)
         learned_code  = self.agent.TACO.module.a_quantizer.embedding.weight
         u = learned_code[code_selected, :]
@@ -167,8 +168,9 @@ class Workspace:
 
     def eval_mt45(self):
         performance = {}
+        # TODO read this from cfg, or maybe we can remove this method; seems obsolete
         task_lst = ['assembly', 'basketball', 'button-press-topdown', 'button-press-topdown-wall', 'button-press', 'button-press-wall', 'coffee-button', 'coffee-pull', 'coffee-push', 'dial-turn', 'disassemble', 'door-close', 'door-open', 'drawer-close', 'drawer-open', 'faucet-open', 'faucet-close', 'hammer', 'handle-press-side', 'handle-press', 'handle-pull-side', 'handle-pull', 'lever-pull', 'peg-insert-side', 'pick-place-wall', 'pick-out-of-hole', 'reach', 'push-back', 'push', 'pick-place', 'plate-slide', 'plate-slide-side', 'plate-slide-back', 'plate-slide-back-side', 'peg-unplug-side', 'soccer', 'stick-push', 'stick-pull', 'push-wall', 'reach-wall', 'shelf-place', 'sweep-into', 'sweep', 'window-open', 'window-close']
-        
+
         for task_name in task_lst:
             env = mw.make(task_name, 3, 2, self.cfg.seed, device_id=self.device, train=False)
             reward_total, success = [], 0
@@ -191,18 +193,18 @@ class Workspace:
             performance[task_name] = success / self.cfg.num_eval_episodes * 100
             if self.rank == 0:
                 print("===============Task:{} Number of Eval Episodes:{} Success Rate:{}%===============".format(task_name, self.cfg.num_eval_episodes, success / self.cfg.num_eval_episodes * 100))
-        
+
         if self.rank == 0:
             eval_dir = self.results_dir / 'eval'
             eval_dir.mkdir(parents=True, exist_ok=True)
             save_dir = eval_dir / '{}.pkl'.format(self.cfg.exp_bc_name)
             with open(save_dir, 'wb') as f:
                 pickle.dump(performance, f)
-    
+
     def eval_st(self):
         #print('=====================Begin Evaluation=====================')
         eval_until_episode = utils.Until(self.cfg.num_eval_episodes)
-        
+
         eval_env, task_name = self.eval_env, self.cfg.task_names[0]
         counter, step, episode, total_reward, success = 0, 0, 0, 0, 0
         while eval_until_episode(episode):
@@ -226,12 +228,12 @@ class Workspace:
         self.performance.append(success/self.cfg.num_eval_episodes*100)
         eval_dir = self.results_dir / 'eval'
         eval_dir.mkdir(parents=True, exist_ok=True)
-        if self.rank == 0:    
+        if self.rank == 0:
             with open(eval_dir / '{}.pkl'.format(self.cfg.exp_bc_name), 'wb') as f:
                 pickle.dump(self.performance, f)
             #print('=======================End Evaluation=======================')
-    
-    def train_tokenizer(self):
+
+    def pretrain_models(self):
         metrics = None
         while self.global_step < self.cfg.num_train_steps:
             if self.global_step%100 == 0 and self.rank == 0:
@@ -258,13 +260,13 @@ class Workspace:
         # BUG: This copy doesn't seem to copy anything. Figure out why.
         distutils.dir_util.copy_tree(str(self.logger._log_dir), str(dest_log_dir))
 
-    
+
     def train_bpe(self):
         self.agent.TACO.train(False)
         lst_traj = []
         for task_dir in self.pretraining_data_dirs:
             lst_traj.extend(utils.choose(list(sorted(task_dir.glob('*.npz'))), self.cfg.max_traj_per_task))
-        
+
         print('Loaded {} trajectories'.format(len(lst_traj)))
         traj_names = []
         ### Train BPE tokenizer
@@ -291,7 +293,7 @@ class Workspace:
                 print(f"Processed {counter} trajectories")
 
         print('=========Offline Data Tokenized!==========')
-        
+
         ### Train tokenizer on the tokenized pretraining trajectories
         tokenizer = Tokenizer(algo='bpe', vocab_size=self.cfg.vocab_size)
         tokenizer.train(corpus, min_frequency=self.cfg.min_frequency, max_token_length=self.cfg.max_token_length, verbose=True)
@@ -301,7 +303,7 @@ class Workspace:
         with open(vocab_dir / 'vocab_mt45_code{}_vocab{}_minfreq{}_maxtoken{}.pkl'.format(self.cfg.n_code, self.cfg.vocab_size, self.cfg.min_frequency, self.cfg.max_token_length), 'wb') as f:
             pickle.dump([tokenizer, corpus, traj_names], f)
 
-    
+
     def train_metapolicy(self):
         metrics = None
         vocab_dir = self.results_dir / 'vocab'
@@ -314,7 +316,7 @@ class Workspace:
         path = construct_task_data_path(self.cfg.data_storage_dir, self.cfg.downstream_task_name, self.cfg.task_data_dir_suffix)
         lst_traj = utils.choose(list(sorted(path.glob('*.npz'))), self.cfg.max_traj_per_task)
         self.tok_to_idx = dict() ### Token, Index Lookup
-        self.idx_to_tok = []    
+        self.idx_to_tok = []
         for f in lst_traj:
             with np.load(f) as e:
                 episode = dict(e)
@@ -327,14 +329,14 @@ class Workspace:
                 _, _, _, _, min_encoding_indices = self.agent.TACO.module.a_quantizer(u)
                 min_encoding_indices = list(min_encoding_indices.reshape(-1).detach().cpu().numpy())
                 min_encoding_indices = [int(idx) for idx in min_encoding_indices]
-            
+
             traj_tok = [self.tokenizer.encode(min_encoding_indices[t:], verbose=False)[0] for t in range(obs.shape[0])]
             for tok in traj_tok:
                 if not tok in self.tok_to_idx:
                     self.tok_to_idx[tok] = len(self.tok_to_idx)
                     self.idx_to_tok.append(tok)
-            
-        
+
+
         self.agent.train(False)
         meta_policy = nn.Sequential(
             nn.Linear(self.cfg.feature_dim, self.cfg.hidden_dim),
@@ -349,13 +351,13 @@ class Workspace:
         self.agent.taco_opt = torch.optim.Adam(self.agent.TACO.parameters(), lr=self.cfg.lr)
         tok_to_code = lambda tok: self.tokenizer.decode([int(tok.item())], verbose=False)[0] ### Token =>  First Code
         tok_to_idx  = lambda tok: self.tok_to_idx[int(tok.item())] ### Token => Index
-        
+
         while self.global_step < self.cfg.num_train_steps:
             if self.global_step%1000 == 0 and self.rank == 0:
                 # wait until all the metrics schema is populated
                 if metrics is not None:
                     # log stats
-                    print('DECODER_LOSS:{}, META_POLICY_LOSS:{}'.format(metrics['decoder_loss'], metrics['meta_policy_loss'])) 
+                    print('DECODER_LOSS:{}, META_POLICY_LOSS:{}'.format(metrics['decoder_loss'], metrics['meta_policy_loss']))
                     elapsed_time, total_time = self.timer.reset()
 
                 # reset env
@@ -365,7 +367,7 @@ class Workspace:
 
             self._global_step += 1
             metrics = self.agent.update_metapolicy(self.replay_iter, self.global_step, tok_to_code, tok_to_idx)
-        
+
             if self.global_step%self.cfg.eval_freq == 0:
                 if len(self.cfg.task_names) == 1:
                     self.eval_st()
@@ -375,22 +377,22 @@ class Workspace:
             if len(self.cfg.task_names) == 1:
                 self.eval_st()
             else:
-                
+
                 self.eval_mt45()
 
-    
+
     def save_snapshot(self, stage):
         if stage == 1:
             snapshot = self.results_dir / 'snapshot.pt'
         else:
             snapshot = self.results_dir / 'snapshot_vocab{}.pt'.format(self.cfg.vocab_size)
-                                                                   
+
         keys_to_save = ['agent', '_global_step']
         payload = {k: self.__dict__[k] for k in keys_to_save}
         #payload = self.agent.TACO.state_dict()
         with snapshot.open('wb') as f:
             torch.save(payload, f)
-            
+
     def load_snapshot(self):
         snapshot = self.results_dir / 'snapshot.pt'
         with snapshot.open('rb') as f:
@@ -402,12 +404,12 @@ class Workspace:
         self.agent.TACO.device = self.device
         self.agent.TACO.to(self.device)
         print('Resuming Snapshopt')
-        
-    
+
+
     def save_encoder(self):
         counter = (self.global_step // 10000) + 1
         snapshot = self.results_dir / "encoder_{}.pt".format(counter)
-        
+
         payload = self.__dict__['agent'].encoder.state_dict()
         with snapshot.open('wb') as f:
             torch.save(payload, f)
@@ -431,13 +433,13 @@ def main(cfg):
         workspace.train_multitask_bc()
     else:
         if cfg.stage == 1:
-            workspace.train_tokenizer()
+            workspace.pretrain_models()
         elif cfg.stage == 2:
             workspace.train_bpe()
         elif cfg.stage == 3:
             workspace.train_metapolicy()
         else:
-            raise Error
+            raise ValueError(f"Invalid stage: {cfg.stage}")
     destroy_process_group()
 
 def wrapper(rank, world_size, cfg):
@@ -447,9 +449,12 @@ def wrapper(rank, world_size, cfg):
     print(f'WORLD SIZE: {world_size}, RANK: {rank}')
     main(cfg)
 
-def main_mp_launch_helper(cfg):
+def main_mp_launch_helper(cfg=None):
     world_size = torch.cuda.device_count()
-    mp.spawn(wrapper, args=(world_size, cfg), nprocs=world_size)
+    if world_size==1:  # single GPU
+        wrapper(0, 1, cfg)  # don't use multiprocessing
+    else:
+        mp.spawn(wrapper, args=(world_size, cfg), nprocs=world_size)
 
 if __name__ == '__main__':
-    main_mp_launch_helper(None)
+    main_mp_launch_helper()
